627ec958ee5e79d4d52cae3f561588d4c8aa13ba
Ava <avasophianorgaard@gmail.com>
2025-07-28 10:13:55 -0700

    added array for to do items

--- todo_proj/ContentView.swift
+++ todo_proj/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftData
 
 struct ContentView: View {
     @State private var showNewTask = false
+    @Query var toDos: [ToDoItem]
     var body: some View {
         VStack {
             HStack {
@@ -29,6 +30,11 @@ struct ContentView: View {
             }
             .padding()
             Spacer()
+            List {
+                ForEach(toDos) { toDoItem in
+                    Text(toDoItem.title)
+                }
+            }
         }
         if showNewTask {
             NewToDoView()
